d475f8e73babf981ed4a7a65d20e9772fdc15d4d
Ravi Kiran Vemana <117162959+Ravikiran000@users.noreply.github.com>
2024-12-12 17:45:28 +0530

    Update api.js

--- src/apiConfig/api.js
+++ src/apiConfig/api.js
@@ -1,3 +1,4 @@
+/*
 import axios from "axios";
 
 const baseUrl = process.env.REACT_APP_BASE_URL;
@@ -5,5 +6,16 @@ const baseUrl = process.env.REACT_APP_BASE_URL;
 const instance = axios.create({
     baseURL: `${baseUrl}`
 });
+
+export default instance;
+*/
+
+import axios from "axios";
+
+console.log("REACT_APP_BASE_URL", process.env.REACT_APP_BASE_URL)
+
+const baseUrl = process.env.REACT_APP_BASE_URL;
+
+const instance = axios.create({baseURL: ${baseUrl}});
 
 export default instance;
